50f6f86f789b53694c1a9466ba3b2412b330427e
Ben Hardcastle <ben.hardcastle@alleninstitute.org>
2024-11-15 07:31:25 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -205,9 +205,6 @@ def main():
     else:
         logger.info(f"Using list of {len(session_ids)} session_ids")
 
-    # pipeline can crash if a results folder is expected and not found
-    pathlib.Path("/results").mkdir(exist_ok=True)
-
     # run processing function for each session, with test mode implemented:
     for session_id in session_ids:
         process_session(session_id, params=params, test=args.test)
@@ -216,4 +213,6 @@ def main():
             break
 
 if __name__ == "__main__":
+    # pipeline can crash if a results folder is expected and not found, and requires creating manually:
+    pathlib.Path("/results").mkdir(exist_ok=True)
     main()
